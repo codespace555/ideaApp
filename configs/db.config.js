4b920350939c20254aa50b7d14a2119d583811d7
codespace555 <namansanjaykumar@gmail.com>
2023-07-09 09:45:11 +0530

    connect to DB

---
+++ configs/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    DB_Name : "idae_app_demo",
+    DB_URL:"mongodb://localhost/idea_app_demo"
+}
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require("express")
-const dotenv = require("dotenv")
+// const dotenv = require("dotenv")
 const serverConfig = require("./configs/server.config.js")
+const dbConfig = require("./configs/db.config.js")
+const mongoose = require("mongoose")
 var app = express()
-app.listen(serverConfig.PORT ,()=>{
+// here connect the monogo db
+mongoose.connect(dbConfig.DB_URL)
+const db = mongoose.connection;
+db.on("error",() => {
+    console.log('Error connecting to database')
+})
+db.once("open",() => {
+console.log("DB is connected")
+})
+app.listen(serverConfig.PORT, () => {
     console.log(`Server running on http://localhost:${serverConfig.PORT}`)
 })
